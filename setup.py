b9a5dda0a82ecc51bbf66c38c312d2a8368c1633
Theo Smith <theo.smith@pentagroup.co>
2025-04-23 23:29:35 +0100

    Windows update to setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,26 @@
 from setuptools import setup, Extension
 from setuptools import find_packages
 import pybind11
+import sys
+import platform
+
+# Extra compiler args depending on platform
+extra_compile_args = ["-std=c++11"]
+extra_link_args = []
+
+if platform.system() == "Darwin":  # macOS
+    extra_compile_args.append("-stdlib=libc++")
+elif platform.system() == "Windows":
+    extra_compile_args = ["/std:c++17"]  # For MSVC
 
 ext_modules = [
     Extension(
         "poker_engine.hand_eval",
         sources=["poker_engine/hand_eval.cpp"],
-        include_dirs=[pybind11.get_include()],
-        language="c++"
+        include_dirs=[pybind11.get_include(), pybind11.get_include(user=True)],
+        language="c++",
+        extra_compile_args=extra_compile_args,
+        extra_link_args=extra_link_args,
     )
 ]
 
